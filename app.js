8628274722f20fc3fa411452661410324ef05eb7
samar sayed <sayedsamar58@gmail.com>
2026-04-16 22:33:47 +0530

    push

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname+"/pages/index.html");
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "index.html"));
 });
 
-app.get("/skills", (req,res) => {
-    res.sendFile(__dirname+"/pages/my_skills.html");
+app.get("/skills", (req, res) => {
+    res.sendFile(path.join(__dirname, "pages", "my_skills.html"));
 });
 
-
-app.listen(port, (err) => {
-    console.log("Server started on http://localhost:"+port);
-})
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
